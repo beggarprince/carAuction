df0df74bb098ff3ad44f32d2d5044a118748e28e
driver <beatyouonetime@gmail.com>
2025-07-05 19:57:45 -0500

    added some more html and registration code

--- src/main/java/aaa/pfa/carAuctionBackend/controller/RegisterCarController.java
+++ src/main/java/aaa/pfa/carAuctionBackend/controller/RegisterCarController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class RegisterCarController {
@@ -16,8 +17,8 @@ public class RegisterCarController {
     }
 
     @GetMapping("/register")
-    public String register(){
-        return "register";
+    public RedirectView register(){
+        return new RedirectView("/register.html");
     }
 
     @PostMapping("/register")
